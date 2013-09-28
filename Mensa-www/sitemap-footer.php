c181b7ca8a1374830c4e84128d28023412e04381
UllaSainio <ulla.sainio@kolumbus.fi>
2013-09-28 11:58:57 +0300

    Mun tekemä footer
    …en nyt vaan pääse läpi sinne mensa-www:lle suoraan
    
    minusta tässä on paremmin sijoiteltu tekstit

---
+++ Mensa-www/sitemap-footer.php
@@ -0,0 +1,81 @@
+<?php
+ /*
+  * Sitemap footer. Used by most pages.
+  *
+  * @package mensafin
+  */
+?>
+
+<div id="footerBG"> 
+  <footer>
+    <div id="footerInfoArea"> 
+<<<<<<< HEAD
+       <div style="width:195px;" class="footerInfo">
+       	<ul>
+           <li style="padding-bottom:5px;" class="footerText">LINKKEJÄ</li>
+           <li class="footerText"><a target="_blank" href="http://www.mensa.org/">Kansainvalinen Mensa</a></li>
+           <li class="footerText"><a target="_blank" href="http://www.facebook.com/Mensantestit">Facebook</a></li>
+           <li class="footerText"><a target="_blank" href="http://twitter.com/intent/follow?screen_name=mensafinland&amp;tw_p=followbutton&amp;variant=2.0">Twitter</a></li>
+           <li class="footerText"><a target="_blank" href="http://www.youtube.com/MensaFinland">YouTube</a></li>
+           <li class="footerText"><a target="_blank" href="http://pinterest.com/mensafinland/">Pinterest</a></li>
+=======
+       <div style="width:170px;" class="footerInfo">
+         <ul>
+           <li style="padding-top:10px;" class="footerText"><a href="http://www.mensa.fi/wordpress/?page_id=85">Medialle</a></li>
+           <!--<li class="footerText"><a href="#">Aineistopankki</a></li>-->
+           <li class="footerText"><a href="http://www.mensa.fi/wordpress/?year=<?php echo date("Y"); ?>">Arkisto</a></li>
+>>>>>>> 4a76c194c44613bbd16689cdf6b95376deadfc74
+         </ul>
+       </div>
+       
+       <div style="width:195px;" class="footerInfo">
+         <ul>
+           <li style="padding-bottom:5px;" class="footerText">YHTEYSTIEDOT</li>
+           <li class="footerText">Suomen Mensa ry</li>
+           <li class="footerText">PL 792</li>
+           <li style="padding-bottom:5px;" class="footerText">00101 Helsinki FINLAND</li>
+           <li class="footerText">+358 50 525 2464</li>
+           <li style="padding-bottom:5px;" class="footerText">email toimisto[at]mensa.fi</li>
+         </ul>
+       </div>
+
+       <div style="width:130px;" class="footerInfo">
+         <ul>
+<<<<<<< HEAD
+           <li style="padding-bottom:5px;" class="footerText">ARKISTO</li>
+           <li class="footerText"><a href="http://www.mensa.fi/wordpress/?page_id=85">Medialle</a></li>
+           <!--<li class="footerText"><a href="#">Aineistopankki</a></li>-->
+           <li class="footerText"><a href="http://www.mensa.fi/wordpress/?year=2013">2012 tms otsikko</a></li>
+=======
+           <li class="footerText"><a target="_blank" href="http://www.mensa.org/">Kansainv&auml;linen Mensa</a></li>
+		   
+           <li class="footerText"><a target="_blank" href="http://www.facebook.com/Mensantestit">Facebook</a></li>
+           <li class="footerText"><a target="_blank" href="https://twitter.com/MensaFinland">Twitter</a></li>
+           <li class="footerText"><a target="_blank" href="http://www.youtube.com/MensaFinland">YouTube</a></li>
+           
+           <li class="footerText"><a target="_blank" href="http://pinterest.com/mensafinland/">Pinterest</a></li>
+>>>>>>> 4a76c194c44613bbd16689cdf6b95376deadfc74
+         </ul>
+       </div>
+
+       <div style="width:240px;" class="footerInfo">
+         <ul>
+           <li class="footerText">&copy; Mensa Finland 2013</li>
+           <li style="padding-bottom:5px;" class="footerText"> All rights reserved</li>
+           <li></li>
+<<<<<<< HEAD
+           <li style="padding-bottom:5px;" class="footerText"> last update 
+           <!-- #BeginDate format:Am1 -->September 18, 2013<!-- #EndDate -->    </li>
+           <li><img alt="" src="images/transparent.png" /></li>
+           <li><img alt="" src="images/transparent.png" /></li>
+           <li class="footerText"><a href="http://www.gentil.fi">design gentil.fi</a></li>
+=======
+           <li style="padding-bottom:5px;" class="footerText"> Viimeisin p&auml;ivitys 26.9.2013</li>
+           <li style="padding-bottom:5px;" class="footerText"><a href="http://www.gentil.fi">Design gentil.fi</a></li>
+>>>>>>> 4a76c194c44613bbd16689cdf6b95376deadfc74
+         </ul>
+       </div><!-- end footerInfo -->
+     </div><!-- end footerInfoArea  -->
+  </footer><!-- end footer --><!-- end footer -->
+</div><!-- end .footerBG -->
+
